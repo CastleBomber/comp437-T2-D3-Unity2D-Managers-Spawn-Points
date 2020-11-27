cb48e298dde73c432d70d785ba93ab9423db3055
CastleBomber <richie.ucsb@gmail.com>
2020-11-27 11:17:26 -0800

    animationGood

--- Unity 2D Managers and Spawn Points/Assets/Scripts/MonoBehaviour/MovementController.cs
+++ Unity 2D Managers and Spawn Points/Assets/Scripts/MonoBehaviour/MovementController.cs
@@ -4,40 +4,41 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    
     public float movementSpeed = 3.0f;
     Vector2 movement = new Vector2();
+
     Animator animator;
+
     string animationState = "AnimationState";
     Rigidbody2D rb2D;
 
     enum CharStates
-	{
+    {
         walkEast = 1,
         walkSouth = 2,
         walkWest = 3,
         walkNorth = 4,
         idleSouth = 5
-	}
+    }
 
-    void Start()
+    private void Start()
     {
         animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
         UpdateState();
     }
 
-	private void FixedUpdate()
-	{
+    void FixedUpdate()
+    {
         MoveCharacter();
-	}
+    }
 
     private void MoveCharacter()
-	{ 
+    {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
@@ -46,30 +47,26 @@ public class MovementController : MonoBehaviour
     }
 
     private void UpdateState()
-	{
-        if (movement.x > 0) {
-            animator.SetInteger(animationState, (int)
-            CharStates.walkEast);
-		}
+    {
+        if (movement.x > 0)
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkEast);
+        }
         else if (movement.x < 0)
-		{
-            animator.SetInteger(animationState, (int)
-            CharStates.walkWest);
-		}
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkWest);
+        }
         else if (movement.y > 0)
-		{
-            animator.SetInteger(animationState, (int)
-            CharStates.walkNorth);
-		}
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkNorth);
+        }
         else if (movement.y < 0)
-		{
-            animator.SetInteger(animationState, (int)
-            CharStates.walkSouth);
-		}
-		else
-		{
-            animator.SetInteger(animationState, (int)
-            CharStates.idleSouth);
-		}
-	}
+        {
+            animator.SetInteger(animationState, (int)CharStates.walkSouth);
+        }
+        else
+        {
+            animator.SetInteger(animationState, (int)CharStates.idleSouth);
+        }
+    }
 }
